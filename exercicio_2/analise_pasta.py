90232e896e48ca70667fcd0ff302874c389c8dfc
Tomás <tomasneto2000@gmail.com>
2021-05-13 23:04:58 +0100

    cant do 2 sadge (falta corrigir exe2 e criar menu)

--- exercicio_2/analise_pasta.py
+++ exercicio_2/analise_pasta.py
@@ -2,35 +2,46 @@ import collections
 import os
 import glob
 import csv
+import re
 from matplotlib import pyplot as plt
+
 
 def pede_pasta():
     while True:
         path = input("Introduza um caminho para uma pasta:")
+        #print(os.path.isdir(f"{os.getcwd()}\\{path}"))
         if os.path.exists(path):
             return path
 
 
 def faz_calculos(dir):
-    # res = {}
-    listaFicheiros = os.listdir(dir)
-    # print(listaFicheiros)
+    #res = {}
+    #listaFicheiros = os.listdir(dir)
+    #print(listaFicheiros)
 
     os.chdir(dir)
     count = collections.Counter()
     volume = collections.Counter()
     for filename in glob.glob("*"):
         name, ext = os.path.splitext(filename)
-        count[ext] += 1
-        volume[ext] += os.path.getsize(filename)
+        if ext != '':
+            count[ext] += 1
+            volume[ext] += os.path.getsize(filename)
     count = {k: ("quantidade: " + str(v)) for k, v in count.items() if k in count}
     volume = {k: ("volume: " + str(v)) for k, v in volume.items() if k in volume}
-    # print(dict(count))
-    # print(dict(volume))
+# print(dict(count))
+# print(dict(volume))
     res = {k: {count[k], v} for k, v in volume.items() if k in count}
 
-    # res = {str(volume.keys()) + "volume: " + str(dict(volume)) + " quantidade: " + str(dict(count)) + "}"}
+# res = {str(volume.keys()) + "volume: " + str(dict(volume)) + " quantidade: " + str(dict(count)) + "}"}
     return res
+
+def aux(res):
+    xd = {key: [re.sub('quantidade:', '', ele) for ele in val]
+    for key, val in res.items()}
+    kek = {key: [re.sub('volume:', '', ele) for ele in val]
+    for key, val in xd.items()}
+    return kek
 
 
 def guarda_resultados(res):
@@ -40,8 +51,10 @@ def guarda_resultados(res):
     print(name)
     with open(name, 'w', newline="") as csv_file:
         writer = csv.writer(csv_file)
-        for key, value in res.items():
-            writer.writerow([key, value])
+        writer.writerow(["Extensao", "Quantidade", "Tamanho[kByte]"])
+        for key, value in aux(res).items():
+            writer.writerow([key,value])
+
 
 
 def faz_grafico_queijos(titulo, lista_chaves, lista_valores):
@@ -60,5 +73,5 @@ if __name__ == '__main__':
     # print(pede_pasta())
     #print(faz_calculos(pede_pasta()))
     x = faz_calculos(pede_pasta())
+    #print(aux(x))
     guarda_resultados(x)
-
